3d38fd979a3e52ff66d47d608d647322c71f470a
jochun <jkchun@protonmail.com>
2021-11-23 17:09:56 -1000

    working quiz application

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,41 @@
 
 // questions!
 var questions = [{
-    question:"What do all HTML documents start with?",
-    answers: ["Hypertext","html","<!DOCTYPE html","<html>"],
+    question: "What do all HTML documents start with?",
+    answers: ["Hypertext", "html", "<!DOCTYPE html", "<html>"],
     correctAnswer: "<!DOCTYPE html"
-    
-},{
-    question:"What is the process of finding errors and fixing them within a program called?",
-    answers: ["Solving","Fixing","Resetting","Debugging"],
+
+}, {
+    question: "What is the process of finding errors and fixing them within a program called?",
+    answers: ["Solving", "Fixing", "Resetting", "Debugging"],
     correctAnswer: "Debugging"
 
-},{
-    question:"How do you see which remote links (in GitHub) have been established for your project",
-    answers: ["git remote links","git remote -v","git add A-","git remote origin"],
+}, {
+    question: "How do you see which remote links (in GitHub) have been established for your project",
+    answers: ["git remote links", "git remote -v", "git add A-", "git remote origin"],
     correctAnswer: "git remote -v"
 
-},{
-    question:"Why do we need an alt attribute for images in an HTML document?",
-    answers: ["To identify the image","To access the image","To give context for the image","To style the image"],
+}, {
+    question: "Why do we need an alt attribute for images in an HTML document?",
+    answers: ["To identify the image", "To access the image", "To give context for the image", "To style the image"],
     correctAnswer: "To give context for the image"
-},{
-    question:"Which CSS property do you use to align the flex container's items on the main-axis?",
-    answers: ["justify-content","align-content","align-text","flex-center"],
+}, {
+    question: "Which CSS property do you use to align the flex container's items on the main-axis?",
+    answers: ["justify-content", "align-content", "align-text", "flex-center"],
     correctAnswer: "justify-content"
-},{
-    question:"The visible part of the HTML document is between:",
-    answers: ["<head> and </head>","<html> and </html>","<body> and </body>","<visible> and </visible"],
+}, {
+    question: "The visible part of the HTML document is between:",
+    answers: ["<head> and </head>", "<html> and </html>", "<body> and </body>", "<visible> and </visible"],
     correctAnswer: "<body> and </body>"
 }]
 
 
 //Setting temporary timer
 //var timer = 10
-var timer = questions.length*10;
+var timer = questions.length * 10;
 
 var questionNumber = 0
-var timerId 
+var timerId
 var timerCountdown = document.getElementById("time")
 var startScreen = document.getElementById("rules-container")
 var questionEl = document.querySelector("#question")
@@ -75,7 +75,7 @@ function endQuiz() {
     endScreen.removeAttribute("class")
     questionEl.setAttribute("class", "hide")
     questionScreen.setAttribute("class", "hide")
-   
+
 }
 
 
@@ -83,31 +83,34 @@ function endQuiz() {
 // function that tells us which question we're on
 function updateQuestion() {
     //check to see if we've reached the end of the array, endQuiz
-   
-     //the questions array object
-     var currentQuestion = questions[questionNumber];
-     var questionsLength = questions.length;
-     for (var i = 0; i < questions.length; i++) {
 
-        if (currentQuestion == questionsLength) 
-            endQuiz();
-     
-     }
-     
-     questionEl.textContent = currentQuestion.question
-     answerList.innerHTML = "";
+    //the questions array object
+    var currentQuestion = questions[questionNumber];
+    var questionsLength = questions.length;
 
-     //for loop answers on screen
-     var answerEl = document.querySelector("#answers");
-     for (var i = 0; i < currentQuestion.answers.length; i++){
-         var liEl = document.createElement("li");
-         var buttonEl = document.createElement("button");
-         buttonEl.textContent = currentQuestion.answers[i]
-         buttonEl.addEventListener("click", checkAnswer )
-         // add button class
-         liEl.append(buttonEl)
-         answerEl.append(liEl)
-     }
+
+    if (questionNumber == questionsLength) {
+        endQuiz();
+    }
+
+    else {
+
+
+        questionEl.textContent = currentQuestion.question
+        answerList.innerHTML = "";
+
+        //for loop answers on screen
+        var answerEl = document.querySelector("#answers");
+        for (var i = 0; i < currentQuestion.answers.length; i++) {
+            var liEl = document.createElement("li");
+            var buttonEl = document.createElement("button");
+            buttonEl.textContent = currentQuestion.answers[i]
+            buttonEl.addEventListener("click", checkAnswer)
+            // add button class
+            liEl.append(buttonEl)
+            answerEl.append(liEl)
+        }
+    }
 }
 
 //create timer, needs to start on button click
@@ -127,7 +130,7 @@ function checkAnswer(event) {
             timer = 0
         }
         timerCountdown.textContent = timer;
-        
+
         // play wrong sound effect
 
         answerStatus.textContent = "Wrong Answer"
@@ -135,17 +138,17 @@ function checkAnswer(event) {
     // if else true, play the right sound and say correct answer
     questionNumber++
     updateQuestion()
-    
+
 }
 
 
 //score and store in localStorage
-var saveScores = function() {
+var saveScores = function () {
     var score = JSON.parse(localStorage.getItem("score")) || [];
     var initials = document.getElementById("initials-input")
-    
+
     var playerInfo = {
-        time: timer, 
+        time: timer,
         initials: initials.value
     }
     score.push(playerInfo)
@@ -157,13 +160,13 @@ var saveScores = function() {
 };
 
 //get localStorage Scores
-var loadScores = function() {
+var loadScores = function () {
     score = JSON.parse(localStorage.getItem("score"));
- // display scores
- var scoreBoardEl = getElementBy("score-board")  
- var scoresList = document.createElement("li");
- scoresList.innerHTML = "<span>" + playerInfo.initials + "</span><h3>" + playerInfo.time + "</h3>",
-scoreBoardEl.appendChild(scoresList);
+    // display scores
+    var scoreBoardEl = getElementBy("score-board")
+    var scoresList = document.createElement("li");
+    scoresList.innerHTML = "<span>" + playerInfo.initials + "</span><h3>" + playerInfo.time + "</h3>",
+        scoreBoardEl.appendChild(scoresList);
 
 };
 
